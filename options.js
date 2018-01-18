2adacbb2d9e4cb815f0b958bedb3d3b1d9985f27
kachsiron <solo_zor@yahoo.com>
2018-01-18 12:17:10 +0300

    Update options.js

--- options.js
+++ options.js
@@ -2595,7 +2595,8 @@ function mChats(){
 				}
 			}.bind(this);
 			this.butRem.onclick=function(){
-				this.data.el.remove();
+				let {chat,n,el}=this.data;
+				el.remove();
 				this.data=null
 			}.bind(this);
 			/*for(let x=0;x<2;x++){
