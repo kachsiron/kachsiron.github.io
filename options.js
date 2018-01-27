2152958f684f0188be5f8e66c5e20cc755825924
kachsiron <solo_zor@yahoo.com>
2018-01-27 15:39:59 +0300

    Update options.js

--- options.js
+++ options.js
@@ -2680,7 +2680,7 @@ function mChats(){
 			pp = pp[1];
 			chat.nickColors2[n] = pp
 		}
-		img.style.backgroundImage = 'url("'+pokemonImage+'")';
+		img.style.backgroundImage = 'url("https://kachsiron.github.io/imgs/canvasPokemon.png")';
 		img.style.backgroundPosition = pokemonPoint[pp][0] + 'px ' + pokemonPoint[pp][1] + 'px'; 
 		bb.forEach(a=>{a.style.color=c})
 	}
