8d55bf15b26d1e5e7afdab8742dea9c57c3696b1
kachsiron <solo_zor@yahoo.com>
2019-02-15 12:11:10 +0300

    Update ftl.js

--- ftl/ftl.js
+++ ftl/ftl.js
@@ -17,17 +17,9 @@ listDiv.style.width='500px';
 listDiv.style.border='1px solid white';
 listDiv.style.borderRadius='15px';
 listDiv.style.padding='15px';
-var imgs=[];var WW=[]
+var imgs=[];
 for(let i=0,l=W.length,mainDiv,tempDiv,imgDiv,img,w,iw,ih,ahash;i<l;i++){
 	w=W[i];
-	if(w.title.includes('√'))continue;
-	if(w.title.includes('†'))continue;
-	if(w.title.includes('^'))continue;
-	if(w.title.includes('‡'))continue;
-	if(w.title.includes('€'))continue;
-	if(w.title.includes('™'))continue;
-	if(w.title.includes('‰'))continue;
-	WW.push(w);
 	mainDiv=document.createElement('DIV');
 	mainDiv.style.clear='both';
 	mainDiv.style.overflowX='hidden';
