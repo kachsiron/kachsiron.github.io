3e537392abd703548771835fbc6f75f9d5ba89e0
kachsiron <solo_zor@yahoo.com>
2019-09-01 21:23:25 +0300

    Update Unit.js

--- stard/Unit.js
+++ stard/Unit.js
@@ -6,7 +6,8 @@ class Unit {
 		this.onMap = false;
 		this.div = document.createElement('DIV');
 		this.div.className = 'unit';
-		this.imgs = { 'face': document.createElement('IMG') };
+		this.imgs = { 'face': document.createElement('DIV') };
+		this.imgs.face.className = 'anim';
 		this.div.appendChild(this.imgs.face);
 		this.par = {};
 		this.anims = {};
@@ -23,9 +24,10 @@ class Unit {
 
 		let anim = this.anims[an]
 		anim.frame[0] = 0;
-		
+		this.imgs.face.style.backgroundImage = 'url(' + anim.img + ')';
+
 		this.animInterval = setInterval(() => {
-			this.imgs.face.src = anim.imgs[ anim.frame[0] ];
+			this.imgs.face.style.backgroundPosition = (-anim.frame[0] * 64) + 'px 0';
 			anim.frame[0]++;
 			if (anim.frame[0] >= anim.frame[1]) anim.frame[0] = 0
 		}, 50);
@@ -33,22 +35,7 @@ class Unit {
 	convert(type) {
 		this.type = type;
 		for (let i in U[type].anim) {
-			this.anims[i] = { 'imgs': [], 'frame': [0, U[type].anim[i].frame] }
-		}
-		for (let i = 0; i < this.anims.stand.frame[1]; i++) {
-			this.anims.stand.imgs.push('img/' + type + '/stand/frame_' + (i + 1).totwo(5) + '.png')
-		}
-		for (let i = 0; i < this.anims.walk_left.frame[1]; i++) {
-			this.anims.walk_left.imgs.push('img/' + type + '/walk_left/frame_' + (i + 1).totwo(5) + '.png')
-		}
-		for (let i = 0; i < this.anims.walk_right.frame[1]; i++) {
-			this.anims.walk_right.imgs.push('img/' + type + '/walk_right/frame_' + (i + 1).totwo(5) + '.png')
-		}
-		for (let i = 0; i < this.anims.walk_up.frame[1]; i++) {
-			this.anims.walk_up.imgs.push('img/' + type + '/walk_left/frame_' + (i + 1).totwo(5) + '.png')
-		}
-		for (let i = 0; i < this.anims.walk_down.frame[1]; i++) {
-			this.anims.walk_down.imgs.push('img/' + type + '/walk_right/frame_' + (i + 1).totwo(5) + '.png')
+			this.anims[i] = { 'img': 'img/' + type + '/' + i + '.png', 'frame': [0, U[type].anim[i].frame] }
 		}
 		
 		this.setAnim('stand');
