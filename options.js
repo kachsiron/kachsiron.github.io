5d283119cd3749020e0d8f2cdefa18f401791163
kachsiron <solo_zor@yahoo.com>
2019-09-27 23:12:49 +0300

    Update options.js

--- options.js
+++ options.js
@@ -2381,7 +2381,7 @@ function mChats(){
 			w.sock.onerror=function(e){console.log('error',e);OPOV.serv('Ошибка в сокете. Смотри в консоль',10000)};
 			w.sock.onmessage=e=>{i.amoTwitch(e,w)};
 			w.sock.onopen=()=>{
-				w.interval=setInterval(function(){this.titleDiv.style.backgroundColor=this.bColor;this.sock.send('PING')}.bind(w),30000);
+				w.interval=setInterval(function(){this.titleDiv.style.backgroundColor=this.bColor;this.sock.send('PING :tmi.twitch.tv')}.bind(w),30000);
 				w.interval2=setInterval(i.getTwitchChatterCount.bind({i:i,w:w}),60000);
 				i.getTwitchChatterCount.call({i:i,w:w});
 				w.sock.send('CAP REQ :twitch.tv/tags twitch.tv/commands');
@@ -2606,6 +2606,7 @@ function mChats(){
 	this.amoTwitchPubsub=function(e,w){console.log(e)}
 	this.amoTwitch=function(e,w){
 //@badge-info=;badges=moderator/1,partner/1;color=#32C3A2;display-name=Streamlabs;emotes=;flags=;id=5a62caba-48a1-409a-b233-6158f91769f6;mod=1;room-id=21255999;subscriber=0;tmi-sent-ts=1569612194226;turbo=0;user-id=105166207;user-type=mod :streamlabs!streamlabs@streamlabs.tmi.twitch.tv PRIVMSG #kritzkast :Thank you for following Crom1337!
+//@badge-info=;badges=;color=;display-name=racchedoon;emotes=160394:17-22,24-29,31-36;flags=;id=9eb63434-03d1-49e6-9e3e-5fd1e5c0eefd;mod=0;room-id=426609374;subscriber=0;tmi-sent-ts=1569613621807;turbo=0;user-id=424781590;user-type= :racchedoon!racchedoon@racchedoon.tmi.twitch.tv PRIVMSG #mariamaggry :чатику приветики BegWan BegWan BegWan
 		console.log('a',e)
 		let r=e.data.match(rgxpChatTwitch[2]);
 		if(r[1]==='@'){
